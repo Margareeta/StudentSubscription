f87473be4b7386dc40eef216f1d600c4c28f698e
Daisy <la.reine.m@gmai.com>
2021-10-12 01:18:52 +0300

    Initial commit

---
+++ src/main/java/com/studentsub/studentsubscription/StudentSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.studentsub.studentsubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studentsub/studentsubscription/StudentSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentsub.studentsubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
